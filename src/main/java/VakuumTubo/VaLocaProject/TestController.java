2a4dca9363301b39e4f7eb38aed0a33dd11619a8
Loca0307 <loca2gaming@gmail.com>
2026-01-06 00:47:41 +0100

    initial basic Controller request

---
+++ src/main/java/VakuumTubo/VaLocaProject/TestController.java
@@ -0,0 +1,15 @@
+package VakuumTubo.VaLocaProject;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class TestController{
+
+    @GetMapping("/")
+    public String getString(){
+        return "Ciao";
+    }
+    
+}
