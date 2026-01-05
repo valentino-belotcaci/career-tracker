dd396d475f68cde94ff5f1dad6f045ae5651e992
Loca0307 <loca2gaming@gmail.com>
2026-01-06 00:29:06 +0100

    inital commit

---
+++ src/test/java/VakuumTubo/VaLocaProject/CareerTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package VakuumTubo.VaLocaProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareerTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
