39dbc54b4f571247d5ac6c15e542a1f4ccbfe055
Loca0307 <loca2gaming@gmail.com>
2026-01-07 11:55:24 +0100

    created accountDTO

--- src/main/java/VaLocaProject/Controllers/UserController.java
+++ src/main/java/VaLocaProject/Controllers/UserController.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import VaLocaProject.Services.UserService;
@@ -12,6 +11,7 @@ import VaLocaProject.Models.User;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 
 @RestController
@@ -26,11 +26,12 @@ public class UserController{
         return ResponseEntity.ok(userService.getAllUsers());
     }
 
-    // Inserts a new user
-    @PostMapping("/User/insertUser/{id}")
-    public ResponseEntity<User> insertUser(@PathVariable User user) {
+    // Inserts a new user (send JSON body without id/version)
+    @PostMapping("/User/insertUser")
+    public ResponseEntity<User> insertUser(@RequestBody User user) {
+        // Ensure server doesn't treat client-sent id as update
+        try { java.lang.reflect.Field f = user.getClass().getDeclaredField("user_id"); f.setAccessible(true); f.set(user, null); } catch (Exception ignored) {}
         return ResponseEntity.ok(userService.saveUser(user));
     }
 
-    
 }
