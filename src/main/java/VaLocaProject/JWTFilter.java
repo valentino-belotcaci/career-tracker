3ae9f39b6fa82ef909b4eb02876cab8f5582d278
Valentino Belotcaci <valentinobelotcaci@gmail.com>
2026-01-15 21:59:08 +0100

    finished implementing jwt auth with some problems

--- src/main/java/VaLocaProject/JWTFilter.java
+++ src/main/java/VaLocaProject/JWTFilter.java
@@ -3,22 +3,35 @@ package VaLocaProject;
 import java.io.IOException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import VaLocaProject.Models.User;
+import VaLocaProject.Services.CustomUserDetailsService;
 import VaLocaProject.Services.JWTService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+@Component
 public class JWTFilter extends OncePerRequestFilter{
 
 
     @Autowired
     private JWTService jwtService;
+
+    @Autowired
+    ApplicationContext context;
+    
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
@@ -35,9 +48,22 @@ public class JWTFilter extends OncePerRequestFilter{
         if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
             //securitycontextif its not null it means its not authenticated yet
             // Logic to set authentication in the security context goes here
-            if (jwtService.validateToken(token, username)) {
+
+            UserDetails userDetails = context.getBean(CustomUserDetailsService.class).loadUserByUsername(username); // Load user details using username
+
+            if (jwtService.validateToken(token, userDetails)) {
                 // Set authentication in the security context
+                UsernamePasswordAuthenticationToken authToken = 
+                    new UsernamePasswordAuthenticationToken(
+                        userDetails, null, userDetails.getAuthorities());  
+
+                authToken.setDetails(
+                    new WebAuthenticationDetailsSource().buildDetails(request));
+
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+            }
         }
+        filterChain.doFilter(request, response);
     }
 
 }
