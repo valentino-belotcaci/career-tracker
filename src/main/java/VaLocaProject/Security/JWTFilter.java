9bb700028adcc0575ecef928db93bda3f3b3cb66
Loca0307 <loca2gaming@gmail.com>
2026-01-17 13:14:40 +0100

    trying to fix authorization problems

--- src/main/java/VaLocaProject/Security/JWTFilter.java
+++ src/main/java/VaLocaProject/Security/JWTFilter.java
@@ -33,6 +33,7 @@ public class JWTFilter extends OncePerRequestFilter{
         String authHeader = request.getHeader("Authorization");
         String token = null;
         String username = null;
+        
 
         if (authHeader != null && authHeader.startsWith("Bearer ")) {
             token = authHeader.substring(7);
