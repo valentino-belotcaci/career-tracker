167efd88e3fccfd9f270f15466684fd3ea3e08ec
Loca0307 <loca2gaming@gmail.com>
2026-01-22 21:37:26 +0100

    .

--- src/main/java/VaLocaProject/Services/CompanyService.java
+++ src/main/java/VaLocaProject/Services/CompanyService.java
@@ -1,5 +1,6 @@
 package VaLocaProject.Services;
 
+import java.time.Duration;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,12 +8,18 @@ import org.springframework.stereotype.Service;
 
 import VaLocaProject.Models.Company;
 import VaLocaProject.Repositories.CompanyRepository;
+import VaLocaProject.Security.RedisService;
 
 @Service
 public class CompanyService {
     
     @Autowired
     CompanyRepository companyRepository;
+
+    @Autowired
+    RedisService redisService;
+
+    private static final Duration COMPANY_CACHE_TTL = Duration.ofHours(1); // Defines lifetime of cache
 
     public List<Company> getAllCompanies(){
         return companyRepository.findAll();
@@ -52,11 +59,46 @@ public class CompanyService {
     }
 
     public Company getCompanyByAccountId(Long id){
-         // Return if else null to resolve Optional
-        return companyRepository.findById(id).orElse(null);
+        String key = "company:" + id;
+
+        // try read from redis first
+        try {
+            Object cached = redisService.get(key);
+            if (cached instanceof Company company)  return company;
+            
+        } catch (Exception ignored) {}
+
+        // fallback to DB
+        Company company = companyRepository.findById(id).orElse(null);
+
+        // cache the DB result if found
+        if (company != null) {
+            try {
+                redisService.save(key, company, COMPANY_CACHE_TTL);
+            } catch (Exception ignored) {}
+        }
+
+        return company;
     }
 
     public Company getCompanyByEmail(String email){
-        return companyRepository.findByEmail(email);
+        String key = "company:" + email;
+
+        try {
+            Object cached = redisService.get(key);
+            if (cached instanceof Company company)  return company;
+
+        } catch (Exception e) {
+        }
+        Company company = companyRepository.findByEmail(email);
+
+        if (company != null){
+            try {
+                redisService.save(key, company, COMPANY_CACHE_TTL);
+            } catch (Exception e) {
+            }
+        }
+
+        return company;
     };
 }
