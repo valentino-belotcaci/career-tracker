126f009d5385bd3110d6faf9c3c37b825585ff74
Loca0307 <loca2gaming@gmail.com>
2026-01-30 11:00:55 +0100

    started tests for jobapplciation controller

---
+++ backend/src/test/java/VaLocaProject/Controllers/JobApplicationControllerTest.java
@@ -0,0 +1,81 @@
+package VaLocaProject.Controllers;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertSame;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.ResponseEntity;
+
+import VaLocaProject.Models.JobApplication;
+import VaLocaProject.Services.JobApplicationService;
+
+
+public class JobApplicationControllerTest {
+
+    @Mock
+    JobApplicationService jobApplicationService;
+
+    @InjectMocks
+    JobApplicationController jobApplicationController;
+
+        // Initialize mocks
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);  
+    }
+
+
+    @Test
+    void testGetAllApplications() {
+        JobApplication application1 = new JobApplication();
+        application1.setApplicationId(1L);
+
+        JobApplication application2 = new JobApplication();
+        application2.setApplicationId(2L);
+
+        List<JobApplication> mockApplications = Arrays.asList(application1, application2);
+
+        when(jobApplicationService.getAllApplications()).thenReturn(mockApplications);
+
+        ResponseEntity<List<JobApplication>> response = jobApplicationController.getAllApplications();
+
+        assertEquals(200, response.getStatusCode().value());
+        assertEquals(2, response.getBody().size());
+        assertEquals(1L, response.getBody().get(0).getApplicationId());
+        assertSame(application2, response.getBody().get(1));
+    }
+
+    @Test
+    void testInsertApplication() {
+        JobApplication application1 = new JobApplication();
+        application1.setApplicationId(1L);
+
+        when(jobApplicationService.insertApplication(application1)).thenReturn(application1);
+
+        ResponseEntity<JobApplication> response = jobApplicationController.insertApplication(application1);
+
+        assertEquals(200, response.getStatusCode().value());
+        assertSame(application1, response.getBody());
+
+        verify(jobApplicationService, times(1)).insertApplication(application1);
+    }
+
+
+    @Test
+    void testDeleteAllApplications() {
+        ResponseEntity<String> response = jobApplicationController.deleteAll();
+
+        assertEquals(200, response.getStatusCode().value());
+        assertEquals("All job applications deleted", response.getBody());
+        verify(jobApplicationService, times(1)).deleteAllApplications();
+    }
+}
