2712f3685cced09cbbaa6b9fe1152efc54c21085
Loca0307 <loca2gaming@gmail.com>
2026-01-30 18:23:20 +0100

    .

--- backend/src/main/java/VaLocaProject/CareerTrackerApplication.java
+++ backend/src/main/java/VaLocaProject/CareerTrackerApplication.java
@@ -2,8 +2,10 @@ package VaLocaProject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class CareerTrackerApplication {
 
 	public static void main(String[] args) {
