b2c0b2004b837a0776ee11347258d4ccddf7e82a
Valentino Belotcaci <valentinobelotcaci@gmail.com>
2026-01-30 22:55:00 +0100

    setup accountServiceTest class

--- backend/src/test/java/VaLocaProject/Services/AccountServiceTest.java
+++ backend/src/test/java/VaLocaProject/Services/AccountServiceTest.java
@@ -1,22 +1,31 @@
 package VaLocaProject.Services;
-/*  
+
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
+import java.util.Arrays;
 import java.util.List;
 
 import VaLocaProject.Models.Account;
 import VaLocaProject.Models.Company;
 import VaLocaProject.Models.User;
+import VaLocaProject.Repositories.CompanyRepository;
 import VaLocaProject.Repositories.UserRepository;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.ResponseEntity;
 
-@ExtendWith(MockitoExtension.class)
-class AccountServiceTest {
+
+class AccountControllerTest {
+
+    @Mock
+    private CompanyRepository companyRepository;
 
     @Mock
     private UserRepository userRepository;
@@ -24,27 +33,10 @@ class AccountServiceTest {
     @InjectMocks
     private AccountService accountService;
 
-    @Test
-    void getAllAccounts_shouldReturnListOfAccounts() throws Exception {
-        // given
-        User user = new User();
-        user.setId(1L);
-
-        Company company = new Company();
-        company.setId(2L);
-
-        when(accountService.getAllAccounts())
-                .thenReturn(List.of(user, company));
-
-        // when + then
-        mockMvc.perform(get("/Account/getAllAccounts"))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.length()").value(2))
-                .andExpect(jsonPath("$[0].id").value(1))
-                .andExpect(jsonPath("$[0].type").value("USER"))
-                .andExpect(jsonPath("$[1].id").value(2))
-                .andExpect(jsonPath("$[1].type").value("COMPANY"));
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
     }
 
+
 }
-*/
