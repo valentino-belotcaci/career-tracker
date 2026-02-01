f328537a53feea110507e181e8e15f921d40fed8
Loca0307 <loca2gaming@gmail.com>
2026-02-01 12:36:15 +0100

    .

--- backend/src/main/java/VaLocaProject/Security/RedisConfig.java
+++ backend/src/main/java/VaLocaProject/Security/RedisConfig.java
@@ -16,13 +16,17 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 public class RedisConfig {
 
     @Bean
+    // Template needed to connect to Redis and perform operations
     RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
 
         RedisTemplate<String, Object> template = new RedisTemplate<>();
+        // Connect to actual redis server
         template.setConnectionFactory(connectionFactory);
 
+        // Define a redis serializer that uses JSON format to trnasalte redis bytes
         RedisSerializer<Object> jsonSerializer = RedisSerializer.json();
 
+        // Set keys and values type for normal and hash operations
         template.setKeySerializer(new StringRedisSerializer());
         template.setValueSerializer(jsonSerializer);
         template.setHashKeySerializer(new StringRedisSerializer());
@@ -31,19 +35,26 @@ public class RedisConfig {
         return template;
     }
 
+
     @Bean
+    // Needed to configure spring cache abstraction with redis
     RedisCacheManager cacheManager(RedisConnectionFactory factory) {
 
         RedisSerializer<Object> jsonSerializer = RedisSerializer.json();
 
+        // Class used to configure redis cache behavior
         RedisCacheConfiguration config =
+        // Based on default cache config of spring
             RedisCacheConfiguration.defaultCacheConfig()
                 .computePrefixWith(name -> name + ":")
+                // Define duration of cache entries
                 .entryTtl(Duration.ofMinutes(30))
+                // Tells redis to use json serializer for values
                 .serializeValuesWith(
                     RedisSerializationContext.SerializationPair.fromSerializer(jsonSerializer)
                 );
 
+        // Creates the actual cache manager
         return RedisCacheManager.builder(factory)
                 .cacheDefaults(config)
                 .build();
