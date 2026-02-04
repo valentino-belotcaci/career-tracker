a524c6d5d94e4ba230e41dbe14d73448b082f3a6
Loca0307 <loca2gaming@gmail.com>
2026-02-04 13:05:05 +0100

    .

--- frontend/src/api/accountApi.ts
+++ frontend/src/api/accountApi.ts
@@ -1,7 +1,8 @@
 import client from "./client";
 import {type Account} from "../types/Account";
 
-export const getAccountById = async (id: number) => {
+// Basic method that takes id : number and returns Promise<Account>
+export const getAccountById = async (id: number) : Promise<Account> => {
   const response = await client.get<Account>(`/Account/getAccountById/${id}`);
   return response.data;
 };
