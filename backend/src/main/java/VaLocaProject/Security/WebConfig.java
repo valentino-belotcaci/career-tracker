bd3d7e19ca1782c9a7fff046439d50667fd0a68d
Loca0307 <loca2gaming@gmail.com>
2026-02-10 19:33:49 +0100

    finished rate limiter

--- backend/src/main/java/VaLocaProject/Security/WebConfig.java
+++ backend/src/main/java/VaLocaProject/Security/WebConfig.java
@@ -17,6 +17,6 @@ public class WebConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         // Limit only sensitive actions, like creating posts or applications
         registry.addInterceptor(rateLimitInterceptor)
-                .addPathPatterns("/JobPost/getAllPosts", "/JobApplication/insertJobApplication");
+                .addPathPatterns("**");
     }
 }
