8a9efaa05a5872203734fb9f82e29a33afe7c674
Valentino Belotcaci <valentinobelotcaci@gmail.com>
2026-02-10 12:17:08 +0100

    register change and change profile

--- frontend/src/components/ProfileUser/ProfileUserComponent.tsx
+++ frontend/src/components/ProfileUser/ProfileUserComponent.tsx
@@ -7,22 +7,36 @@ export default function ProfileUserComponent(){
 
     const { loggedId } = Context(); //get authentication data(id of the logged in user)
 
-    const [profile, setProfile] = useState<Account | null>(null) //User | null  means, at start null, after fetch become User Object.
+    const [profile, setProfile] = useState<Partial<Account>>({}); //partial acc good for editable forms
     //profile holds user data, set profile to update it when we clicke edit
     const [isEditing, setIsEditing] = useState(false); //false view mode, true edit mode
 
     useEffect(() => { //we fetch the data after the component renders
+        
         const fetchUser = async () => {
 
-        if (!loggedId) //at this point its defenitely a string, if  not we go back
-            return;
+            if (!loggedId) //at this point its defenitely a string, if  not we go back
+                return;
 
-        const data = await getAccountById(loggedId);
-        setProfile(data);
-        };
+            const data = await getAccountById(loggedId);
+            setProfile(data);
+            };
 
-        if (loggedId) fetchUser();
-    }, [loggedId]);
+            if (loggedId) fetchUser();
+
+        }, [loggedId]);
+
+        const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {//It contains everything about the change that just happened.
+
+            if (!profile) return;
+
+            setProfile({ //setProfile updates react state
+                ...profile,
+                [e.target.name]: e.target.value //e.target is the input element itself that triggered the change.
+        });
+};
+
+    
 
     
 }
