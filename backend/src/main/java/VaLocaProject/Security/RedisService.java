0f7b2c11856b4c7c57e68fd5d7fe0de95d4b7f66
Loca0307 <loca2gaming@gmail.com>
2026-02-10 19:27:25 +0100

    trying to implement redis rate limiter

--- backend/src/main/java/VaLocaProject/Security/RedisService.java
+++ backend/src/main/java/VaLocaProject/Security/RedisService.java
@@ -5,7 +5,7 @@ import java.time.Duration;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
-// IMPOERTANT:
+// IMPORTANT:
 // NOW OBSOLETE AS WE USE SPRING CACHE ABSTRACTION WITH REDIS
 @Service
 public class RedisService {
