7ed19d2da7fafc213f44f8b55ca5856e30d57607
Loca0307 <loca2gaming@gmail.com>
2026-02-10 19:12:54 +0100

    .

--- backend/src/main/java/VaLocaProject/Security/SecurityConfig.java
+++ backend/src/main/java/VaLocaProject/Security/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -20,6 +21,7 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 
 @Configuration
+@EnableWebSecurity // Explicitly tell spring here there is a security configuration 
 public class SecurityConfig {
 
     private final JWTFilter jwtFilter;
